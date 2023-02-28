e117f1ea031f9d5d85af1b9bda3aa235e967750c
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2023-02-28 18:57:02 +0200

    add redux devtools functionality

--- src/store.js
+++ src/store.js
@@ -5,6 +5,10 @@ const reducers = combineReducers({
   api: apiReducer,
 });
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : compose;
+
+const store = createStore(reducers, composeEnhancers());
 
 export default store;
