6bb68beb8a7bf50571b916b7a4aaa2bb9d9c2eab
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2023-03-01 13:41:18 +0200

    add Material UI for styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
 import { Provider } from 'react-redux';
 import store from 'store';
 import App from './App';
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <CssBaseline />
       <App />
     </Provider>
   </React.StrictMode>
